b6f66151d8ee4dfcbf2c14705575d23c74df9f56
ekuisma <eelis.kuisma@gmail.com>
2025-04-14 19:59:28 +0300

    Update e2_2.py

--- exercise-2/e2_2.py
+++ exercise-2/e2_2.py
@@ -17,7 +17,7 @@ display = SSD1306_I2C(WIDTH,HEIGHT,i2c)
 fbuf = framebuf.FrameBuffer(bytearray(WIDTH * HEIGHT * 1), WIDTH, HEIGHT, framebuf.MONO_VLSB)
 # the button
 button = Pin(15, Pin.IN, Pin.PULL_UP)
-s = 1 # the status of the button
+previous_button_state = 1 # the status of the button
 
 def get_input():
     if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
@@ -31,9 +31,9 @@ while True:
         if len(messages) > 8:
            messages.pop(0)
            
-    if button.value() != s:
-        s = button.value()
-        if s == 0:
+    if button.value() != previous_button_state:
+        previous_button_state = button.value()
+        if previous_button_state == 0:
             messages = []
     
     display.fill(0)
@@ -44,5 +44,3 @@ while True:
         display.blit(fbuf, 0, y, 0)
         y += 8
     display.show()
-    
-    time.sleep(0.1)

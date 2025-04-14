d374ec521ec441474084e199bd76c0f458bca424
ekuisma <eelis.kuisma@gmail.com>
2025-04-14 19:57:43 +0300

    Update e2_1.py

--- exercise-2/e2_1.py
+++ exercise-2/e2_1.py
@@ -5,7 +5,7 @@ import time
 
 WIDTH =128
 HEIGHT= 64
-name = "My Name Here"
+name = "My Name"
 name_width = len(name) * 8
 name_height = 8
 name_x = int((WIDTH - name_width) / 2)
@@ -16,15 +16,21 @@ i2c=I2C(0,scl=Pin(17),sda=Pin(16),freq=200000)
 #Initialize the OLED display
 display = SSD1306_I2C(WIDTH,HEIGHT,i2c)
 #Update the frame buffer with some text
-fbuf = framebuf.FrameBuffer(bytearray(WIDTH * HEIGHT * 1), WIDTH, HEIGHT, framebuf.MONO_VLSB)
+# fbuf = framebuf.FrameBuffer(bytearray(WIDTH * HEIGHT * 1), WIDTH, HEIGHT, framebuf.MONO_VLSB)
+show_text = True
 
 button = Pin(15, Pin.IN, Pin.PULL_UP)
 previous_button_state = 1 # the status of the button
 while True:
     if button.value() != previous_button_state:
         previous_button_state = button.value()
-        if previous_button_state == 0:
+        if previous_button_state == 0 and show_text:
             display.fill(0)
-            fbuf.text(name, name_x, name_y)
-            display.blit(fbuf, 0, 0, 0)
+            display.text(name, name_x, name_y)
             display.show()
+            show_text = False
+        elif previous_button_state == 0 and not show_text:
+            display.fill(0)
+            display.show()
+            show_text = True
+        time.sleep(0.1)
